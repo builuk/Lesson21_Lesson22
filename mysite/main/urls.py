b78cb9d0eb97dd601d5c0c437d188370aff84d3b
aabuiluk <andrii.a.builuk@gmail.com>
2025-11-17 17:38:06 +0200

    Simple app

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello_view
+from .views import hello_view, user_status_view
 
 urlpatterns = [
     path('hello/', hello_view, name='hello'),
+    path('status/', user_status_view, name='user_status'),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def hello_view(request):
     context = {'name': 'Andrii'}
     return render(request, "main/hello.html", context)
+
+def user_status_view(request):
+    is_logged_in = True
+    return render(request, "main/user_status.html", {'is_logged_in': is_logged_in})
